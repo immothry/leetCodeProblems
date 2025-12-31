ed37fc8b886d00c9c4de2e316a2b10055d73fa51
immothry <ashkabofficial.com>
2025-12-31 11:09:20 +0530

    Initial commit

---
+++ customPush_function.js
@@ -0,0 +1,13 @@
+const arr = [1,2,3,4,5,6,7,8,9]
+
+function custompush(arr,element){
+   arr[arr.length]= element 
+   return 
+
+}
+
+
+custompush(arr,30)
+custompush(arr, 30)
+
+console.log(arr);
---
+++ levelx_newyear_calculation.js
@@ -0,0 +1,14 @@
+// let amount = 1500
+// let levelx = amount/2
+// let studentCount =17
+// let studentShare = levelx/studentCount
+
+// console.log(studentShare)
+
+function cakeShare(amount,students){
+    let levelx = amount/2
+    let studentShare = levelx/students
+    return studentShare
+}
+
+console.log(cakeShare(1500, 17));
